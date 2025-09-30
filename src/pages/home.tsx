eba9a1b94bf54cd7f30979cdd4d5c0704d27a0a3
lgaxd <lucasgrilloxd@gmail.com>
2025-09-30 01:38:38 -0300

    feat: Corrigindo a página home e completando a lógica de criação de sessão

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,18 @@
-export function Home(){
+import type { StudySession } from "../types/StudySession";
+import { SessaoList } from "../components/sessao-lista";
+
+interface HomeProps {
+    sessoes: StudySession[];
+    removeSessao: (id: string) => void;
+}
+
+export function Home({ sessoes, removeSessao }: HomeProps) {
     return (
-        <>
-            <p className="p-4 font-bold">Bem vindo ao StudyLogs</p>
-            
-        </>
-    )
+        <div className="container mx-auto p-6">
+            <h1 className="text-3xl font-bold text-blue-800 mb-6">
+                Minhas Sessões de Estudo
+            </h1>
+            <SessaoList sessoes={sessoes} removeSessao={removeSessao} />
+        </div>
+    );
 }
